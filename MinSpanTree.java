468230e145b339a57d3b0a9fb522d8cfca8de052
hewittmd97 <35052258+hewittmd97@users.noreply.github.com>
2018-11-30 14:42:20 -0600

    Add files via upload

--- MinSpanTree.java
+++ MinSpanTree.java
@@ -9,10 +9,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 /**
  *
- * @author hewit
+ * @author hewitt
  */
 public class MinSpanTree {
 
@@ -28,6 +29,11 @@ public class MinSpanTree {
             return false;
         }
     }
+    
+//    public boolean createTree(DisjSets dis, int possibleEdge)
+//    {
+//        int rt1 = dis.find(possibleEdge);
+//    }
     
     public static void main(String[] args) throws IOException
     {
@@ -45,57 +51,128 @@ public class MinSpanTree {
         File file = new File(name);
         BufferedReader read = new BufferedReader(new FileReader(file));
         String input;
-        int numTrees = 0;
-        City cityTrees[] = new City[numTrees];
+        String city1 = null;
+        String city2;
+        int count = 0;
+        DistanceAndCities dAC[] = null;
+        
         while((input = read.readLine()) != null)
         {
-            numTrees++;
-            System.out.println(numTrees);
-            City tempTree[] = cityTrees;
-            cityTrees = new City[numTrees];
-            cityTrees = tempTree;
-            City newCity = new City();
-            newCity.root = true;
-            newCity.name = input.substring(0, input.indexOf(",") - 1);
-            input = input.substring(input.indexOf("," + 1), input.length());
-            cityTrees[numTrees - 1] = new City();
-            cityTrees[numTrees - 1] = newCity;
+            boolean firstTwoCities = true;
             while(!"".equals(input))
             {
-                newCity = new City();
-                newCity.name = input.substring(0, input.indexOf(",") - 1);
-                input = input.substring(input.indexOf("," + 1), input.length());
-                int miles;
-                try
+                count++;
+                if(firstTwoCities == true && count == 1)
                 {
-                    miles = Integer.parseInt(input.substring(0, input.indexOf(",") - 1));
-                    input = input.substring(input.indexOf("," + 1), input.length());
+                    city1 = input.substring(0, input.indexOf(","));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    city2 = input.substring(0, input.indexOf(","));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    int dist = Integer.parseInt(input.substring(0, input.indexOf(",")));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    DistanceAndCities temp[] = new DistanceAndCities[1];
+                    temp[0] = new DistanceAndCities();
+                    temp[0].distance = dist;
+                    temp[0].start = city1;
+                    temp[0].stop = city2;
+                    city1 = city2;
+                    dAC = temp;
+                    firstTwoCities = false;
                 }
-                catch(Exception e)
+                else if(firstTwoCities == true)
                 {
-                    miles = Integer.parseInt(input);
-                    input = "";
-                }
-                if(cityTrees[numTrees - 1].child == false)
-                {
-                    cityTrees[numTrees - 1].child = true;
-                    cityTrees[numTrees - 1].addPointer(newCity, miles);
-                    newCity.parent = true;
-                    newCity.addPointer(cityTrees[numTrees - 1], miles);
+                    city1 = input.substring(0, input.indexOf(","));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    city2 = input.substring(0, input.indexOf(","));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    int dist = Integer.parseInt(input.substring(0, input.indexOf(",")));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    DistanceAndCities temp[] = new DistanceAndCities[count];
+                    System.arraycopy(dAC, 0, temp, 0, dAC.length);
+                    temp[count - 1] = new DistanceAndCities();
+                    temp[count - 1].start = city1;
+                    temp[count - 1].stop = city2;
+                    temp[count - 1].distance = dist;
+                    city1 = city2;
+                    dAC = temp;
+                    firstTwoCities = false;
                 }
                 else
                 {
-                    City parentCity = cityTrees[numTrees - 1].pointer[0];
-                    while(parentCity.child == true)
+                    city2 = input.substring(0, input.indexOf(","));
+                    input = input.substring(input.indexOf(",") + 1, input.length());
+                    int dist;
+                    try
                     {
-                        parentCity = parentCity.pointer[1];
+                        dist = Integer.parseInt(input.substring(0, input.indexOf(",")));
+                        input = input.substring(input.indexOf(",") + 1, input.length());
                     }
-                    parentCity.child = true;
-                    parentCity.addPointer(newCity, miles);
-                    newCity.parent = true;
-                    newCity.addPointer(parentCity, miles);
+                    catch(Exception e)
+                    {
+                        dist = Integer.parseInt(input);
+                        input = "";
+                    }
+                    DistanceAndCities temp[] = new DistanceAndCities[count];
+                    System.arraycopy(dAC, 0, temp, 0, dAC.length);
+                    temp[count - 1] = new DistanceAndCities();
+                    temp[count - 1].start = city1;
+                    temp[count - 1].stop = city2;
+                    temp[count - 1].distance = dist;
+                    city1 = city2;
+                    dAC = temp;             
                 }
             }
         }
+             
+        PriorityQueue pq = new PriorityQueue(count);    //create priority queue
+        for(int i = 0; i < count; i++)                  //add all the weights to the pq
+        {
+            pq.add(dAC[i].distance);
+        }
+        int poll;                                       //used to poll numbers out of pq
+        int counter = count;                            //decrement each time dAC is resized to slowly shrink it
+        int next = 0;                                   //keeps track of the next index that newArray will take
+        DistanceAndCities temp = new DistanceAndCities(); //used to compare weight from pq and distance between cities
+        DistanceAndCities newArray[] = new DistanceAndCities[count];    //this will be the sorted array
+        for(int i = 0; i < count; i++)                                  //initialize each instance of the array
+        {
+            newArray[i] = new DistanceAndCities();
+        }
+        
+        while(dAC.length >= 1)   //while dAC has more than or equal to 1 element in the array
+        {
+            poll = (int)pq.poll();  //gets lowest number from pq and then removes the number from pq. number comes in form of object, so it is cast as an int.
+            temp = dAC[0];      //get the first element of the array
+            int index = 0;      //keep track of what element temp represents.
+            while(temp.distance != poll)    //while the distance in temp does not match the lowest number from the pq.
+            {
+                index++;                    //increment the index 
+                temp = dAC[index];          //get new element for comparison
+            }
+            newArray[next] = temp;          //add the lowest distance and corresponding cities to the new array
+            next++;                         //increment so that next time, the next element in newArray will be replaced
+            counter--;                      //dAC loses 1 element and will be reduced
+            DistanceAndCities tempArray[] = new DistanceAndCities[counter]; //used to reduce size of dAC
+            if(index == 0)                  //if the element with the lowest number was found at index 0
+            {
+                System.arraycopy(dAC, 1, tempArray, 0, dAC.length - 1); //copy every element in dAC to tempArray except for the first element
+            }
+            else
+            {
+                System.arraycopy(dAC, 0, tempArray, 0, index);      //copy every element up to the index of element to be removed. index is not included
+                System.arraycopy(dAC, index + 1, tempArray, index, counter - index);    //copy every element after the index of removed element. index is not included
+            }
+            dAC = tempArray;    //tempArray which has 1 less element than dAC is not copied to dAC
+        }
+        dAC = newArray; //the newly sorted array is then assigned to dAC
+        
+        for(int i = 0; i < dAC.length; i++) //for each element in dAC
+        {
+            System.out.println(i + ": " + dAC[i].start + "<--" + dAC[i].distance + "-->" + dAC[i].stop);    //print the info
+        }
+        
+        DisjSets ds = new DisjSets(count);
+        int numEdges = 0;
+        
     }
 }
